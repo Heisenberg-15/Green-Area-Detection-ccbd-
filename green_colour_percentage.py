2f399f373093dd7097510367b597a6eb706b64f4
mohantyabhijit074 <51369559+mohantyabhijit074@users.noreply.github.com>
2020-04-25 17:23:11 +0530

    Updated green_colour_percentage.py

--- green_colour_percentage.py
+++ green_colour_percentage.py
@@ -1,23 +1,22 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
+import os
+import glob
 
-
-img = cv2.imread('2.jpg')
-
-
-grid_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.figure(figsize=(16,6))
-plt.imshow(grid_RGB) # Printing the original picture after converting to RGB
-
-
-grid_HSV = cv2.cvtColor(grid_RGB, cv2.COLOR_RGB2HSV) # Converting to HSV
-lower_green = np.array([18,20,20])
-upper_green = np.array([125,255,255])
-
-mask= cv2.inRange(grid_HSV, lower_green, upper_green)
-res = cv2.bitwise_and(img, img, mask=mask) # Generating image with the green part
-
-print("Green Part of Image")
-plt.figure(figsize=(16,6))
-plt.imshow(res)
+img_dir = "dataset" # Enter Directory of all images 
+data_path = os.path.join(img_dir,'*g')
+files = glob.glob(data_path)
+data = []
+for f1 in files:
+    img = cv2.imread(f1)
+    grid_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    grid_HSV = cv2.cvtColor(grid_RGB, cv2.COLOR_RGB2HSV) # Converting to HSV
+    lower_green = np.array([18,20,20])
+    upper_green = np.array([105,255,255])
+    mask= cv2.inRange(grid_HSV, lower_green, upper_green)
+    green_perc = (mask==255).mean()
+    if green_perc>=0.75:
+        data.append((f1,green_perc))
+print(data)    
